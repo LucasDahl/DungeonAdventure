069019e3a890850cd40a3b4ef8ff292ba41c094b
Lucas Dahl <LucasDahl425@yahoo.com>
2022-07-26 08:25:01 -0700

    Added the sneakAttack method for the Thief.

--- src/Thief.java
+++ src/Thief.java
@@ -21,11 +21,25 @@ public class Thief extends Hero {
     public Thief(final String theName) {
         super(theName, 75, 6, 0.8, 20, 40, 0.4, 1);
         MY_SPECIAL_SKILL = "Crushing Blow";
-        Random rand = new Random();
         MY_SPECIAL_CHANCE = 0.4;
     }
 
     // **************************** Methods ***************************
+
+    /**
+     *  This special skill will allow the Thief
+     *  character to possible get another attack,
+     *  but can also lose an attack during this turn.
+     */
+    public void sneakAttack() {
+        Random rand = new Random();
+        double chance = rand.nextDouble();
+        if(MY_SPECIAL_CHANCE / 2.0 < chance) {
+            super.setNumberOfAttacks(0);
+        } else if(MY_SPECIAL_CHANCE > chance) {
+            super.setNumberOfAttacks(getNumberOfAttacks() + 1);
+        }
+    }
 
     //========
     // Getters
