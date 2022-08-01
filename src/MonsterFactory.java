e4f54d07e97db1983a9fc5b9c9708f8fab056aaa
Lucas Dahl <LucasDahl425@yahoo.com>
2022-07-31 21:18:36 -0700

    Added missing javadocs fo the factory classes.

--- src/MonsterFactory.java
+++ src/MonsterFactory.java
@@ -1,17 +1,25 @@
 /**
+ * @author lucasdahl -> LDahl
+ * @version 1.0
+ *
+ * This class is a Hero Factory that creates
+ * Heros.
  *
  */
 public class MonsterFactory {
 
     /**
+     * This method will create a Monster.
      *
-     * @param theMonster
-     * @return
+     * @param theMonster the name of the monster.
+     * @return the created monster
      */
     public Monster createMonster(final String theMonster) {
+
         if(theMonster == null || theMonster.isEmpty()) {
             return null;
         }
+
         switch (theMonster) {
             case "Ogre":
                 return new Ogre();
