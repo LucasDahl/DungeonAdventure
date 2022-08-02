cd79e148a7b829f66882345eff7e67537a4634de
Lucas Dahl <LucasDahl425@yahoo.com>
2022-08-02 11:49:05 -0700

    Fixed parameters to follow naming conventions

--- src/DungeonCharacter.java
+++ src/DungeonCharacter.java
@@ -27,13 +27,13 @@ public abstract class DungeonCharacter {
      *  This is the default constructor that
      *  will initialize the fields with passed in values.
      */
-    protected DungeonCharacter(final String the_name, final double the_health, final double the_damage_range_min, final double the_damage_range_max, final double the_attack_speed, final double the_hit_chance, final int theNumberOfAttacks) {
-        myName = the_name;
-        myHealthPoints = the_health;
-        MY_DAMAGE_MIN = the_damage_range_min;
-        MY_DAMAGE_MAX = the_damage_range_max;
-        MY_ATTACK_SPEED = the_attack_speed;
-        MY_CHANCE_TO_HIT = the_hit_chance;
+    protected DungeonCharacter(final String theName, final double theHealth, final double theDamageRangeMin, final double theDamageRangeMax, final double theAttackSpeed, final double theHitChance, final int theNumberOfAttacks) {
+        myName = theName;
+        myHealthPoints = theHealth;
+        MY_DAMAGE_MIN = theDamageRangeMin;
+        MY_DAMAGE_MAX = theDamageRangeMax;
+        MY_ATTACK_SPEED = theAttackSpeed;
+        MY_CHANCE_TO_HIT = theHitChance;
         myNumberOfAttacks = theNumberOfAttacks;
         myRandom = new Random();
     }
@@ -46,12 +46,8 @@ public abstract class DungeonCharacter {
      *
      * @return The status of the characters life.
      */
-    public boolean isDead() {
-        if(myHealthPoints <= 0) {
-            return true;
-        } else {
-            return false;
-        }
+    protected boolean isDead() {
+        return (myHealthPoints <= 0);
     }
 
     //========
@@ -106,13 +102,13 @@ public abstract class DungeonCharacter {
     // Setters
     //========
 
-    protected void setNumberOfAttacks(final int the_number_of_attacks) {
-        myNumberOfAttacks = the_number_of_attacks;
+    protected void setNumberOfAttacks(final int theNumberOfAttacks) {
+        myNumberOfAttacks = theNumberOfAttacks;
     }
 
     // This method will set the health
-    protected void setHealth(final double the_health) {
-        myHealthPoints = the_health;
+    protected void setHealth(final double theHealth) {
+        myHealthPoints = theHealth;
     }
 
     // Sets the name
@@ -129,7 +125,7 @@ public abstract class DungeonCharacter {
 
     /**
      * This is the attack method for one turn.
-     *
+     * flesh this out here
      * @param theOther this is the other character object
      */
     public abstract void attackBehavior(final DungeonCharacter theOther);
